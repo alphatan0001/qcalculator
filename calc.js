c8c96150bba6d1fb0d7044ced5950173603c9526
alphatan0001 <151167721+alphatan0001@users.noreply.github.com>
2024-01-31 19:27:00 +0530

    Update calc.js
    added javascript functions

--- calc.js
+++ calc.js
@@ -1,4 +1,9 @@
 function mycalc(){
-    var ans=document.getElementById('inner1').innerHTML
-    return eval(ans)
+    document.getElementById('inner1').innerHTML=eval(document.getElementById('inner1').innerHTML)
+}
+function myval(a){
+    document.getElementById('inner1').innerHTML=document.getElementById('inner1').innerHTML+String(a)
+}
+function myclear(){
+    document.getElementById('inner1').innerHTML=""
 }
